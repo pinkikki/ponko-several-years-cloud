484b128dae575f6d89c55ef6015e5caac245f181
pinkikki <w-mizutani@pinkikki.jp>
2018-03-18 22:20:22 +0900

    fix test class

--- ponko-several-years-ohagi/src/test/java/jp/pinkikki/PonkoSeveralYearsOhagiApplicationTests.java
+++ ponko-several-years-ohagi/src/test/java/jp/pinkikki/PonkoSeveralYearsOhagiApplicationTests.java
@@ -1,4 +1,4 @@
-package jp.pinkikki.ponkoseveralyearseurekaclient;
+package jp.pinkikki;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PonkoSeveralYearsEurekaClientApplicationTests {
+public class PonkoSeveralYearsOhagiApplicationTests {
 
 	@Test
 	public void contextLoads() {
